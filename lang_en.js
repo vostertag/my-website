8a05137adeabec1dded8e8043a711002f915cefe
Victor <victor.ostertag@gmail.com>
2018-02-15 19:26:53 +0000

    changes

--- lang_en.js
+++ lang_en.js
@@ -5,57 +5,66 @@ var en = {
   "me_contacter_header" : "Contact",
   "introduction" : "A place to learn more about me, my passions and my projects",
   "titre_mes_projets" : "My projects",
-  "resume_p1" : "Project <b>made for ISEP</b>. The goal was to create a web application that will ease <b>the communication between students and teachers</b> "
-  + "I was part of the \"Opossums' group\" (that you can see in this picture) and together, we have sold and created this app."
-  + "<br><br>"
-  + "This project is <b>probably on of the best I have done so far in my studies</b>. Working with that group was really fun "
-  + "and we did our best to produce a website that we are proud of.",
-  "resultat_p1" : "18.5/20 for the marketing, 17.4/20 for the development",
-  "date_p1" : "April 2017 - June 2017",
-  "competences_p1" : "Teamwork, scrum, customer relationship, HTML, CSS, JavaScript/jQuery, Spring Boot, Bootstrap, many libraries",
-  "nom_p2" : "YouTube Channel",
-  "resume_p2" : "I run a YouTube channel on which I talk mainly about <b>video games</b> "
-  + "but also <b>any subject that I can find interesting</b> "
-  + "like history, science, random facts or computing for example."
-  + "<br><br>"
-  + "Running a channel alone is <b>addictive and exciting</b>: you have to be a writer, an actor, a director, an editor, a marketer,... "
-  + "Like programming, you have to learn a lot of things by your own, which I love.",
-  "resultat_p2" : "+100 000 subscribers, +9 million views",
-  "date_p2" : "Since July 2011, when I was 15",
-  "competences_p2" : "Sony Vegas, Premiere Pro, After Effects, Photoshop, writing, filming, SEO, community management, ...",
-  "nom_p3" : "This website",
-  "resume_p3": "This website's goal is to <b>show what I've done so far</b> and to give you a <b>quick look at who I am</b>... Moreover, I think it is "
-  + "important for a developer to have a portfolio."
-  + "<br></br>"
-  + "Coding this website also gave me <b>the opportunity to practice</b>, more precisely the front-end aspect of things. "
-  + "I hope you like it!",
-  "resultat_p3" : "My mom likes it... (that's the only feedback I got so far)",
-  "date_p3" : "July, 12th 2017 - July, 13th 2017",
-  "competences_p3" : "HTML, CSS, JavaScript/jQuery, Bootstrap, some libraries",
-  "nom_p4" : "Oral presentations",
-  "resume_p4" : "<b>I really enjoy making oral presentations</b> and I really pay attention to the aspect of my slides. I think "
-  + "that they must contain <b>few words, and be used to grab attention</b> rather than "
-  + "serving as notes like it is too often the case."
-  + "<br><br>"
-  + "I've put a <b>link to a website with most of my slides</b>. I strongly recommand you the ones on New-Zealand. You "
-  + "can have a look at slides from other projects that I did not include in this website for now as well.",
-  "resultat_p4" : "I always get positive feedbacks on my slides at the end of a presentation",
-  "date_p4" : "Since highscool, probably",
-  "competences_p4" : "Oral presentation, Photoshop, research and explain what you've found",
-  "nom_p5" : "Photography",
-  "resume_p5" : "It's <b>pretty recent</b> but not surprising since I already love making videos. I'm far from being a pro, "
-  + "I still have a lot to learn!"
-  + "<br><br>"
-  + "<b>All the photos on this website are taken by me</b>. If you wish to see some more, you can check out my instagram, "
-  + "the link is below.",
-  "date_p5" : "Since July 2017",
-  "competences_p5" : "Photography, Photoshop",
+  "instruction" : "For more informations on a project, click on it!",
+  "title_p1": "ISEPReact",
+  "resume_p1": "<b>ISEPReact</b> is a website that will ease <b>the communication between students and teachers</b>." 
+  + " I was part of the \"Opossums' group\" (that you can see in this picture) and together, we have sold and created this app.",
+  "date_p1": "April 2017 - June 2017",
+  "competences_p1": "Scrum, HTML, CSS, JavaScript, Spring Boot, Bootstrap",
+  "title_p2": "Solving the heat equation",
+  "resume_p2": "C++ program to <b>solve the heat equation</b> using various schemes (FTCS, Richardson, DuFort-Frankel, Laasonen, Crank-Nicholson)"
+  + " and that <b>creates .dat files and gnuplot commands</b> to display the solution on a graph.",
+  "date_p2": "November 2017 - December 2017",
+  "competences_p2": "C++, computational methods",
+  "title_p3": "Parisian subway",
+  "resume_p3": "Java program that <b>creates a graph of the parisian subway</b>, <b>displays a simplified "
+  + "map of it</b> on screen and <b>allows the user to check the shortest and longest path between two stations</b>.",
+  "date_p3": "May 2017 - June 2017",
+  "competences_p3": "Java, graph theory, JavaScript, HTML, CSS",
+  "title_p4": "Taxi call terminal",
+  "resume_p4": "C program for Unix that <b>simulates a taxi call terminal</b>. One process <b>reads the upcoming calls</b>"
+  + " and send them through a message queue to the other processes which are <b>acting as taxis</b>, taking each call "
+  + "when they are available.",
+  "date_p4": "January 2018",
+  "competences_p4": "C, Unix, parellel programming",
+  "title_p5": "Shaders and OpenGL",
+  "resume_p5": "C++ program coded with Qt that <b>creates a cube using shaders</b>. "
+  + "The user can <b>interact with it</b> by <b>changing its color</b>, <b>his point of view</b> "
+  + "and <b>rotating it about a line that he defined</b>.",
+  "date_p5": "January 2018",
+  "competences_p5": "C++, Qt, OpenGL, Shaders",
+  "title_p6": "ShareMyNotes",
+  "resume_p6": "<b>Website developed in one week</b> and deployed on AWS. It allows teachers to <b>create courses and publish notes</b>, "
+  + "while students can <b>access these notes</b>, <b>create their own</b> and <b>create/join groups</b> to share their work.",
+  "date_p6": "February 2018",
+  "competences_p6": "Bootstrap, JavaScript, PHP, Laravel, MySQL, AWS",
+  "title_p7": "Filler game",
+  "resume_p7": "<b>Filler game</b> coded in Java. Can be <b>played with up to 4 players</b>, "
+  + "<b>against human players or AIs</b>. It is also possible to <b>add obstacles</b>, "
+  + "<b>a fog of war</b> and to <b>create your own map</b>.",
+  "date_p7": "May 2017 - June 2017",
+  "competences_p7": "Java, creating a graphical interface",
+  "title_p8": "This website",
+  "resume_p8": "Website <b>created with boothstrap</b>, <b>some libraries</b> and <b>JavaScript</b>. "
+  + "There is no back-end since there is no need for one here. This website's goal is to <b>introduce " 
+  + "who I am and what I do</b>.",
+  "date_p8": "July 2017",
+  "competences_p8": "HTML, CSS, Bootstrap, JavaScript/jQuery",
+  "title_p9": "YouTube channel",
+  "resume_p9": "I run a YouTube channel since I was 15 on which I talk mainly about <b>video games</b> "
+  + "but also <b>any subject that I can find interesting</b>. It now has <b>over 100 000 subscribers and 9 million views</b>.",
+  "date_p9": "Since July 2011",
+  "competences_p9": "Video and photo editing, writing, filming,  SEO",
+  "title_p10": "Oral presentations",
+  "resume_p10": "<b>I really enjoy making oral presentations</b> and I really pay attention to the aspect of my slides. "
+  + "I think that they must contain <b>few words</b>, and be used to <b>grab attention</b> rather than serving as notes "
+  + "like it is too often the case.",
+  "competences_p10": "Oral presentation, Photoshop",
   "qui_suis_je_titre" : "About me",
   "question1" : "When was I born? <span>October, 9th 1995</span>",
-  "question2" : "Where can you find me? <span>Studying at Cranfield !",
-  "question3" : "Where do I study? <span>At Cranfield and at ISEP",
-  "question4" : "My experience? <span>I was a web developer for Equinoa and I have done quite a lot of projects during my studies.</span>",
-  "question5" : "What do I want to do with my life? <span>I'm not sure! I think that to find out what really drives you, "
-  + "you must try out as much things as possible. So that's what I intend to do, and if I really enjoy somthing, I guess I'll have found the answer to this question!</span>",
+  "question2" : "Where can you find me? <span>Studying at Cranfield!",
+  "question3" : "Where do I study? <span>At Cranfield (not far away from London) and at ISEP (in Paris).",
+  "question4" : "My work experience? <span>I was a web developer for Equinoa during a 6 moths internship.</span>",
+  "question5" : "Am I available for hire? <span>Yes! I am looking for an internship in software engineering starting in may 2018.</span>",
   "question6" : "Do you want to know more about me? <span>Contact me!</span>",
 }
